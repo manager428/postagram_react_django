f912342917c375e333655413b0f6b1b537cd26b4
manager428 <sweetbigdream428@gmail.com>
2023-10-09 06:37:18 +0900

    user register

--- django-api/core/routers.py
+++ django-api/core/routers.py
@@ -1,10 +1,11 @@
 from rest_framework import routers
 from core.user.viewsets import UserViewSet
+from core.auth.viewsets import (RegisterViewSet)
 router = routers.SimpleRouter()
-# ##################################################################### #
-# ################### USER                       ###################### #
-# ##################################################################### #
+
 router.register(r'user', UserViewSet, basename='user')
+router.register(r'auth/register', RegisterViewSet, basename='auth-register')
+
 urlpatterns = [
     *router.urls,
 ]
